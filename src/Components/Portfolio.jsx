f1d883171e1727cf7c9d21580b085c5fc2bd3f29
saorero <91612002+saorero@users.noreply.github.com>
2023-09-11 10:27:05 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/portfolio.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "random pic";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "A web developer",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "I design and develop random pages to enhance my skills, perfected in REACT as my go to framework",
+    url: "https://github.com/saorero",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Ambassadorship Programmes",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "As a way of giving back to society, i have ventured into the art of making an impact through various ambassadorship programmes that i have joined.",
+    url: "https://www.linkedin.com/in/shidmer-orero-8b703323a/",
   },
   {
-    title: "My Resume Site",
+    title: "My Portfolio",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Some of the projects that have worked on as displayed on my github.",
+    url: "https://github.com/saorero",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "As a graphic Designer",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "I have ventured into making beautiful posters that are appealing and worth captivating throgh Canva.",
+    url: "https://canva.com",
   },
 ];
 
